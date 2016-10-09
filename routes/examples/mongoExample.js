102109db68caedadaba2e883a9d9cc047eaa2eb2
Robert <robert@devtailor.com>
2016-10-09 16:45:11 +0300

    mysql plugin added

---
+++ routes/examples/mongoExample.js
@@ -0,0 +1,27 @@
+const Boom = require('boom');
+
+module.exports = {
+	path: '/examples/mongo',
+	handler: (request, reply) => {
+		if(!request.mongo || !request.mongo.db) {
+			reply(Boom.notImplemented('MySql not added'));
+		} else {
+			// get the first document or create one
+			// then increment count field
+			// return count field value to user
+
+			const mongoOptions = {
+				upsert: true,
+				returnOriginal: false
+			};
+
+			request.mongo.db.collection('example').findOneAndUpdate({}, { $inc: { count: 1 } }, mongoOptions, (err, res) => {
+				if(err) {
+					reply(Boom.badImplementation('Bad Mongo, Bad!'));
+				} else {
+					reply({ count: res.value.count });
+				}
+			});
+		}
+	}
+};
