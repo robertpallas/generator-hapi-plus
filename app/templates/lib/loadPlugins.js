d299e4644a5fa9dfd00478a79306547128233390
Robert <robert@devtailor.com>
2016-10-16 14:06:13 +0300

    minor: format tabs->spaces

--- app/templates/lib/loadPlugins.js
+++ app/templates/lib/loadPlugins.js
@@ -19,124 +19,124 @@ const config = require('../config.js');
 const plugins = [Inert, Vision];
 
 const logSqueezeArgs = [{
-	log: '*',
-	response: '*',
-	request: '*',
-	'request-internal': '*'
+    log: '*',
+    response: '*',
+    request: '*',
+    'request-internal': '*'
 }];
 
 plugins.push({
-	register: Good,
-	options: {
-		reporters: {
-			console: [{
-				module: 'good-squeeze',
-				name: 'Squeeze',
-				args: logSqueezeArgs
-			}, {
-				module: 'good-console',
-				args: [{
-					format: 'HH:mm:ss DD.MM.YYYY'
-				}]
-			}, 'stdout'],
-			file: [{
-				module: 'good-squeeze',
-				name: 'Squeeze',
-				args: logSqueezeArgs
-			}, {
-				module: 'good-squeeze',
-				name: 'SafeJson'
-			}, {
-				module: 'rotating-file-stream',
-				args: [
-					'log',
-					{
-						interval: '1d',
-						compress: 'gzip',
-						path: './logs'
-					}
-				]
-			}]
-		}
-	}
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: logSqueezeArgs
+            }, {
+                module: 'good-console',
+                args: [{
+                    format: 'HH:mm:ss DD.MM.YYYY'
+                }]
+            }, 'stdout'],
+            file: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: logSqueezeArgs
+            }, {
+                module: 'good-squeeze',
+                name: 'SafeJson'
+            }, {
+                module: 'rotating-file-stream',
+                args: [
+                    'log',
+                    {
+                        interval: '1d',
+                        compress: 'gzip',
+                        path: './logs'
+                    }
+                ]
+            }]
+        }
+    }
 });
 <% if(postgre) { %>
 plugins.push({
-	register: Pg,
-	options: {
-		cn: config.postgreSql
-	}
+    register: Pg,
+    options: {
+        cn: config.postgreSql
+    }
 });<% } if(mysql) { %>
 
 plugins.push({
-	register: MySql,
-	options: config.mySql
+    register: MySql,
+    options: config.mySql
 });<% } if(mongo) { %>
 
 plugins.push({
-	register: Mongo,
-	options: {
-		url: config.mongo,
-		decorate: true
-	}
+    register: Mongo,
+    options: {
+        url: config.mongo,
+        decorate: true
+    }
 });<% } %>
 
 plugins.push({
-	register: MrHorse,
-	options: {
-		policyDirectory: path.join(__dirname, 'policies'),
-		defaultApplyPoint: 'onPreHandler'
-	}
+    register: MrHorse,
+    options: {
+        policyDirectory: path.join(__dirname, 'policies'),
+        defaultApplyPoint: 'onPreHandler'
+    }
 });
 
 plugins.push({
-	register: Routes,
-	options: {
-		routes: './routes/**/*.js'
-	}
+    register: Routes,
+    options: {
+        routes: './routes/**/*.js'
+    }
 });
 
 plugins.push({
-	register: Swagger,
-	options: {
-		documentationPage: true, // boolean to enable/disable Swagger
-		info: {
-			title: 'API Documentation',
-			version: Pack.version
-		},
-		jsonEditor: true
-	}
+    register: Swagger,
+    options: {
+        documentationPage: true, // boolean to enable/disable Swagger
+        info: {
+            title: 'API Documentation',
+            version: Pack.version
+        },
+        jsonEditor: true
+    }
 });
 <% if(auth) { %>
 module.exports = server => new Promise((resolve, reject) => {
-	server.register(JWT, (jwtRegErr) => {
-		if(jwtRegErr) {
-			reject(jwtRegErr);
-		} else {
-			server.auth.strategy('jwt', 'jwt', true, {
-				key: config.authKey,
-				verifyOptions: {
-					algorithms: ['HS256']
-				},
-				validateFunc
-			});
+    server.register(JWT, (jwtRegErr) => {
+        if(jwtRegErr) {
+            reject(jwtRegErr);
+        } else {
+            server.auth.strategy('jwt', 'jwt', true, {
+                key: config.authKey,
+                verifyOptions: {
+                    algorithms: ['HS256']
+                },
+                validateFunc
+            });
 
-			server.register(plugins, (err) => {
-				if(err) {
-					reject(err);
-				} else {
-					resolve();
-				}
-			});
-		}
-	});
+            server.register(plugins, (err) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        }
+    });
 });<% } else { %>
 module.exports = server => new Promise((resolve, reject) => {
-	server.register(plugins, (err) => {
-		if(err) {
-			reject(err);
-		} else {
-			resolve();
-		}
-	});
+    server.register(plugins, (err) => {
+        if(err) {
+            reject(err);
+        } else {
+            resolve();
+        }
+    });
 });<% } %>
